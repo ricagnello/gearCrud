adc5aeeff98ff95438f514da660e8e06bcba8c1f
Ricardo <ricardo.agnello@bol.com.br>
2021-02-14 20:35:33 -0300

    method DELETE implemented

--- gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
+++ gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,12 @@ public class GearResource {
 
 		return gear.get();
 
+	}
+	
+	//DELETE method
+	@DeleteMapping("/gears/{id}")
+	public void deleteGear(@PathVariable Long id) {
+		gearRepository.deleteById(id);
 	}
 
 }
