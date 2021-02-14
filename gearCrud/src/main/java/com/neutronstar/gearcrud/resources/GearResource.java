665f20eefda38478f704917e193188345fda1230
Ricardo <ricardo.agnello@bol.com.br>
2021-02-14 20:50:05 -0300

    method POST implemented

--- gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
+++ gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
@@ -1,13 +1,18 @@
 package com.neutronstar.gearcrud.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.neutronstar.gearcrud.entities.Gear;
 import com.neutronstar.gearcrud.exceptions.GearNotFoundException;
@@ -41,6 +46,15 @@ public class GearResource {
 	@DeleteMapping("/gears/{id}")
 	public void deleteGear(@PathVariable Long id) {
 		gearRepository.deleteById(id);
+	}
+	
+	//POST method
+	@PostMapping("/gears")
+	public ResponseEntity<Object> createGear(@RequestBody Gear gear){
+		Gear savedGear = gearRepository.save(gear);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedGear.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
