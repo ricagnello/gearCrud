6d408834cd1d80ee0bf0e2a8aa62bec74c92c0e8
Ricardo <ricardo.agnello@bol.com.br>
2021-02-14 13:31:21 -0300

    method GET gear_by_id implemented

--- gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
+++ gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
@@ -1,24 +1,39 @@
 package com.neutronstar.gearcrud.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neutronstar.gearcrud.entities.Gear;
+import com.neutronstar.gearcrud.exceptions.GearNotFoundException;
 import com.neutronstar.gearcrud.repositories.GearRepository;
 
 @RestController
 public class GearResource {
-	
+
 	@Autowired
 	private GearRepository gearRepository;
-	
-	//GET method
+
+	// GET method
 	@GetMapping("/gears")
 	public List<Gear> getAllGears() {
 		return gearRepository.findAll();
+	}
+
+	// Get method by Id
+	@GetMapping("gears/{id}")
+	public Gear getGearById(@PathVariable Long id) {
+		Optional<Gear> gear = gearRepository.findById(id);
+
+		if (!gear.isPresent())
+			throw new GearNotFoundException("id: " + id + " not found");
+
+		return gear.get();
+
 	}
 
 }
