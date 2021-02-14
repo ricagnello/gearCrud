0b69d2620f9d650b427ceef3b311f8f888b7773f
Ricardo <ricardo.agnello@bol.com.br>
2021-02-14 12:02:14 -0300

    project created

---
+++ gearCrud/src/main/java/com/neutronstar/gearcrud/GearCrudApplication.java
@@ -0,0 +1,13 @@
+package com.neutronstar.gearcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearCrudApplication.class, args);
+	}
+
+}
---
+++ gearCrud/src/test/java/com/neutronstar/gearcrud/GearCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutronstar.gearcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
