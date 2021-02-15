bc2075df33a7d7971b8d80d91e994591c55232e2
Ricardo <ricardo.agnello@bol.com.br>
2021-02-14 21:00:38 -0300

    method PUT implemented

--- gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
+++ gearCrud/src/main/java/com/neutronstar/gearcrud/resources/GearResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -55,6 +56,22 @@ public class GearResource {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(savedGear.getId()).toUri();
 		return ResponseEntity.created(location).build();
+	}
+	
+	//PUT method
+	@PutMapping("/gears/{id}")
+	public ResponseEntity<Object> updateGear(@RequestBody Gear gear, @PathVariable Long id){
+		
+		Optional<Gear> gearOptional = gearRepository.findById(id);
+		
+		if(!gearOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		gear.setId(id);
+		
+		gearRepository.save(gear);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
